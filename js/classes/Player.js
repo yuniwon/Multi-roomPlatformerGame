353ee820676647e95547c6aa0795fc0b22150046
yuniwon <dnjsdndus@gmail.com>
2024-01-22 18:11:39 +0900

    modified: 중력효과 적용

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -4,6 +4,10 @@ class Player {
       x: 100,
       y: 100,
     }
+    this.velocity = {
+      x: 0,
+      y: 0,
+    }
     this.width = 100;
     this.height = 100;
     this.sides = {
@@ -11,18 +15,22 @@ class Player {
       // left: this.x,
       // right: this.x + this.width,
     }
-    // this.speed = 1;
+    this.gravity = 0.2;
   }
 
-  draw() { 
+  draw() {
     c.fillStyle = 'red';
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
-  update() { 
-    if (this.sides.bottom < canvas.height) { // 바닥에 닿지 않았으면
+  update() {
+    this.position.y += this.velocity.y;
+    if (this.sides.bottom + this.velocity.y < canvas.height) { // 바닥에 닿지 않았으면
+      this.velocity.y += this.gravity;
       this.position.y += 1; // y = y + 1; 사각형이 아래로 내려가는 것처럼 보임
       this.sides.bottom = this.position.y + this.height; // 바닥에서 멈추게 하려면 bottom = y + height; 를 써야함
+    }else {
+      this.velocity.y = 0;
     }
   }
 }
